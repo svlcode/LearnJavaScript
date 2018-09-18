3e6b3e408a97952edf84e488f8059185a55dc7e7
Vio Slaniceanu <vio.slaniceanu@gmail.com>
2018-09-18 22:13:09 +0300

    Added BlackjackApp

---
+++ BlackJackApp/index.js
@@ -0,0 +1,6 @@
+let card1 = "Ace of Spades",
+    card2 = "Ten of Hearts";
+
+console.log("Welcome to Blackjack!");
+
+console.log("You've dealt: " + card1 + " and " + card2);
---
+++ JavaScript01/index.js
@@ -0,0 +1,5 @@
+let a = 12, b = 5;
+
+var result = Math.floor(a/b); 
+
+console.log(window.result);
