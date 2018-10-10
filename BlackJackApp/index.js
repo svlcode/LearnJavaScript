c05e4b993e1137f8e336a122d89224ef7537e827
CareCarr-Netrom <carecarr@netrom.ro>
2018-10-10 15:05:24 +0300

    Added createDeck and getNextCard functions

--- BlackJackApp/index.js
+++ BlackJackApp/index.js
@@ -1,16 +1,27 @@
 let suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'];
 let values = ['Ace', 'King', 'Queen', 'Jack', 'Ten', 'Nine', 'Eight', 'Seven', 'Six', 'Five', 'Four', 'Three', 'Two'];
 
-let deck = [];
 
-for (let suitIdx = 0; suitIdx < suits.length; suitIdx++) {
-   for (let valueIdx = 0; valueIdx < values.length; valueIdx++) {
-       deck.push(values[valueIdx] + ' of '+ suits[suitIdx]);
-   }
+function createDeck(){
+    let deck = [];
+    for (let suitIdx = 0; suitIdx < suits.length; suitIdx++) {
+        for (let valueIdx = 0; valueIdx < values.length; valueIdx++) {
+            deck.push(values[valueIdx] + ' of '+ suits[suitIdx]);
+        }
+     }
+     return deck;
 }
 
-for (let i = 0; i < deck.length; i++) {
-    console.log(deck[i]);
+function getNextCard(){
+    return deck.shift();
+}
+
+let deck = createDeck();
+
+let playerCards = [getNextCard(), getNextCard()];
+
+for (let i = 0; i < playerCards.length; i++) {
+    console.log(playerCards[i]);
     
 }
 
