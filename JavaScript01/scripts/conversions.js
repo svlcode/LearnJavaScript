a07189e5ee4b1eae3c56b9839bf8ef1fbb74cfc3
Vio Slaniceanu <vio.slaniceanu@gmail.com>
2018-11-04 17:37:22 +0200

    added conversions

---
+++ JavaScript01/scripts/conversions.js
@@ -0,0 +1,7 @@
+export function run(){
+
+    console.log(Number.parseInt('42'));
+    console.log(Number.parseInt('aaa42'));
+    console.log(Number.parseInt('42.55aa'));
+    console.log(Number.parseFloat('42.55aa'));
+}
