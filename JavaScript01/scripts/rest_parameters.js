a5d9d095da3f11be697894915455b9362d1e624f
Vio Slaniceanu <vio.slaniceanu@gmail.com>
2018-11-04 17:38:50 +0200

    added rest parameters demo

--- JavaScript01/scripts/rest_parameters.js
+++ JavaScript01/scripts/rest_parameters.js
@@ -1,5 +1,12 @@
 export function run(){
 
-    
-   
+    //rest paramters is a modern feature of JS which allows a function to store multiple arguments into an array.
+    function sendCars(dayOfWeek, number, ...allCarIds){
+        console.log('today is ' + dayOfWeek);
+        console.log('my number is ' + number);
+        console.log('handling the rest parameter...');
+        allCarIds.forEach(id => console.log(id));
+    }
+
+    sendCars('monday', 2, 1, 3, 5);
 }
