4637ab475b25b2b3a812d16863217600f8fca3be
Vio Slaniceanu <vio.slaniceanu@gmail.com>
2018-11-04 17:37:51 +0200

    added typeof demo

---
+++ JavaScript01/scripts/typeOf.js
@@ -0,0 +1,9 @@
+export function run() {
+    typeof(1); // number
+    typeof(true); //boolean
+    typeof(function(){}); // function
+    typeof({}); // object
+    typeof(null); // object
+    typeof(undefined); // undefined
+    typeof(NaN); // number
+}
