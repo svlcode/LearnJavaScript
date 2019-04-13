2f66d9962866ba8472b8177da851b1346143234e
Vio Slaniceanu <vio.slaniceanu@gmail.com>
2019-04-13 11:43:06 +0300

    added mockAPI call

--- src/scripts/ajax.js
+++ src/scripts/ajax.js
@@ -1,10 +1,21 @@
-function loadDoc() {
+window.document.getElementById('call-mockApi').addEventListener('click', function(){
+    ajaxCall("http://5cb19b048bdfe80014e981dd.mockapi.io/myapi/v1/users");
+});
+
+window.document.getElementById('file-content').addEventListener('click', function(){
+    ajaxCall("ajax_info.txt");
+});
+
+// XMLHttpRequest (XHR)
+function ajaxCall(url) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function(){
         if(this.readyState == 4 && this.status == 200){
-            document.getElementById('demo').innerHTML = this.responseText;
+            // sets the html of an element with the retrieved response
+            document.getElementById('placeholder').innerHTML = this.responseText;
         }
     };
-    xhttp.open("GET", "ajax_info.txt",true);
+    // ajax_info.txt can be replaced with an url
+    xhttp.open("GET", url, true);
     xhttp.send();
 }
