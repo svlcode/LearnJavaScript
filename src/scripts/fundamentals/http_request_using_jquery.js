07873ee1336da06e2fcc658a7da4715cdbc2f364
Vio Slaniceanu <vio.slaniceanu@gmail.com>
2019-04-28 17:24:53 +0300

    add http_request using jquery

---
+++ src/scripts/fundamentals/http_request_using_jquery.js
@@ -0,0 +1,10 @@
+// import * as $ from 'jquery';
+
+// export function run() {
+//     let promise = $.get("http://5cb19b048bdfe80014e981dd.mockapi.io/myapi/v1/users");
+//     promise.then(
+//         data => console.log('logging...' + data),
+//         error => console.log('error...' + error)
+//     );
+    
+// }
